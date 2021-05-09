5803f56c872c485914b5ec6d266e87af33c46825
DaniSan0508-project <d.santosc@terra.com.br>
2021-05-09 15:43:15 -0300

    tabela users criada

---
+++ config/tabelas.js
@@ -0,0 +1,22 @@
+class Tabelas{
+    init(connection){
+        this.connection = connection;
+        this.createUserTable();
+    };
+
+    createUserTable(){
+        const sql = `CREATE TABLE Users (
+            id int NOT NULL AUTO_INCREMENT, 
+            email varchar(100) NOT NULL, 
+            password varchar(100) NOT NULL, 
+            PRIMARY KEY(id))`
+
+            this.connection.query(sql, (err)=>{
+                if(err){
+                    console.log(err)
+                }
+                console.log('Tabela Users criada com sucesso');
+            });
+    };
+};
+export default new Tabelas;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import connection from './config/connection'
+import connection from './config/connection';
+import tabelas from './config/tabelas';
 import app from './app';
 
 connection.connect(err=>{
     if(!err){
         console.log('conectado ao MySQL');
+        tabelas.init(connection);
         app.listen(3333,()=>{
         console.log('servidor rodando');
 });
