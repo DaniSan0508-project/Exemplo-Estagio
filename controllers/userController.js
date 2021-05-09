bdf496ce997161cd66fce3887f6fdef61f6aafec
DaniSan0508-project <d.santosc@terra.com.br>
2021-05-09 16:47:44 -0300

    adicionado validacoes para campo de email e senha

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,22 @@ import User from '../model/User';
 
 class UserController{
     createUser(req,res){
-        let {email,passsword} = req.body
-        const user = req.body;
-
-        User.create(user)
+     
+        const {email,password} = req.body;
+        const user = {}
+            if(email != '' && email != undefined){
+                user.email = email;
+                if(password != '' && password != undefined){
+                    user.password = password;
+                }else{
+                   return res.status(400).json({err:"password inválido!"})
+                }
+            }else{
+               return res.status(400).json({err:"email inválido!"});
+            };
+       
+        
+        User.create(user,res);
     };
 };
 
--- model/User.js
+++ model/User.js
@@ -1,14 +1,13 @@
 import connection from '../config/connection';
 
 class User{
-    create(user){
+    create(user,res){
         const sql = `INSERT INTO Users SET ?`
-        connection.query(sql, user,(erro,res)=>{
-            if(erro){
-                console.log(erro)
-            }else{
-                console.log(res)
-            }
+        connection.query(sql, user,(err,result)=>{
+            if(err){
+                res.status(400).json(err);
+            };
+               res.status(200).json(result);
         });
     };
 };
